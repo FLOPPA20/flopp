fe2b39171d84a65140e12493264ed61809fb10b6
Шлеппик <aleshap302011@gmail.com>
2024-02-18 16:33:01 +0300

    1.0

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ def ban_user(message):
          # сохранение id и статуса пользователя, отправившего сообщение
         user_id = message.reply_to_message.from_user.id
         user_status = bot.get_chat_member(chat_id, user_id).status 
-         # проверка пользователя21321123
+         # проверка пользователя
         if user_status == 'administrator' or user_status == 'creator':
             bot.reply_to(message, "Невозможно забанить администратора.")
         else:
